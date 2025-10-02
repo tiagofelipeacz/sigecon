d4fc0dd8222fc60fb536d314e476d2bf6abbb454
Tiago <falecomprimepublicidade@gmail.com>
2025-10-02 15:15:39 -0300

    chore: ignorar zzzz_Envia

--- zzzz_Envia/resources/views/admin/concursos/_public_link.blade.php
+++
@@ -1,7 +0,0 @@
-@section('content')
-﻿@php
-    $url = \Route::has('site.concursos.show') ? route('site.concursos.show', $concurso->id) : url('/concursos/'.$concurso->id);
-@endphp
-<a href="{{ $url }}" class="btn btn-sm btn-outline-success ms-1" target="_blank" rel="noopener">Ver publico</a>
-
-@endsection
